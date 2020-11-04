71b08c3a6fcdc9cbc737374ecb73aa2e62748cb8
Kenny Lin <kennylin119>
2020-11-04 18:37:43 -0500

    made modifications to support tickets

--- Lottery2.sol
+++ Lottery2.sol
@@ -3,40 +3,61 @@ pragma solidity ^0.4.17;
 contract Lottery {
  address public manager;
  address[] public players;
+
+//  functions for ticketing system
+mapping (address => uint26) public winnings; 
+
+ address[] public tickets;
+ uint public ticketCount = 0;
+ uint public ticketPrice = 0.1; 
+ uint256 public randomNum = 0;
+ address public latestWinner; 
  
  // constructor : as soon as creating contract, it has been also called
  function Lottery() public {
- // msg is a global function
- manager = msg.sender; // Person who create Contract
+    // msg is a global function
+    manager = msg.sender; // Person who create Contract
  }
- 
+
  // sending Eth to enter loggery: payable function type
  function enter() public payable{
- require(msg.value > .01 ether); // some requirements satisfied, then go through next
- players.push(msg.sender);
+    require(msg.value >= ticketPrice ether); // some requirements satisfied, then go through next
+    players.push(msg.sender);
+    for (int i = 0; i < ticketCount; i++){
+        tickets.push(msg.sender);
+    }
+    ticketCount += msg.value / ticketPrice;
  }
  
  function random() private view returns (uint) {
- // global function sha algorism: keccak256
- return uint(keccak256(block.difficulty, now, players));
+    // global function sha algorism: keccak256
+    return uint(keccak256(block.difficulty, now, players));
  }
  
  function pickWinner() public restricted {
- require(msg.sender == manager); // only manager can pick a winner
- 
- uint index = random() % players.length;
- players[index].transfer(this.balance);
- // reset the players array and (0) is initial size
- players = new address[](0);
+    require(ticketCount > 0);
+    require(msg.sender == manager); // only manager can pick a winner
+    
+    randomNum = uint(block.blockhash(block.number - 1)) % ticketCount;
+    latestWinner = tickets[randomNum];
+    uint amtLot = ticketCount * ticketPrice;
+    
+    // uint index = random() % players.length;
+    // players[index].transfer(this.balance);
+    players[index].transfer(amtLot);
+
+    // reset the players array and (0) is initial size
+    players = new address[](0);
+    ticketCount = 0
  }
  
  // validation logic
  modifier restricted() {
- require(msg.sender == manager); // only manager can access
- _; // run all the rest of the code inside that function
+    require(msg.sender == manager); // only manager can access
+    _; // run all the rest of the code inside that function
  }
  
  function getPlayers() public view returns (address[]) {
- return players;
+    return players;
  }
 }
